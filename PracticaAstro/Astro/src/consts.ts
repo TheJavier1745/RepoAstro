28789af0136f03f2a8015066613404bebe30dbdb
TheJavier1745 <javierdiazyevenes2002@gmail.com>
2025-01-02 15:14:20 -0300

    Add files via upload

---
+++ PracticaAstro/Astro/src/consts.ts
@@ -0,0 +1,5 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = 'Consultora AP';
+export const SITE_DESCRIPTION = 'Sitio Web Consultora AP';

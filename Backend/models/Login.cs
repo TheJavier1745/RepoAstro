8bf525246eb7aad9af969e415a07d3f706400025
TheJavier1745 <javierdiazyevenes2002@gmail.com>
2025-01-13 11:25:13 -0300

    intento login parte 32

---
+++ Backend/models/Login.cs
@@ -0,0 +1,8 @@
+namespace Backend.Models
+{
+    public class Login
+    {
+        public string Correo { get; set; } = string.Empty;
+        public string Contrasena { get; set; } = string.Empty;
+    }
+}

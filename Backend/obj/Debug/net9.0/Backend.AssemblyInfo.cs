9e2c2152154ee2a387a1e49e3842afd076b3d5d0
TheJavier1745 <javierdiazyevenes2002@gmail.com>
2025-02-07 11:34:05 -0300

    cambios al layout

--- Backend/obj/Debug/net9.0/Backend.AssemblyInfo.cs
+++ Backend/obj/Debug/net9.0/Backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5325fa227dc1e85a90bbbed868dd07adeb86e402")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+11a7aeb768ebf5a663cfc05677ca39907738a622")]
 [assembly: System.Reflection.AssemblyProductAttribute("Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

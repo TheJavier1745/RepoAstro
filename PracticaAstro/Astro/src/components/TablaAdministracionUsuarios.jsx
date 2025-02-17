e6982fb34d261352d172eb17662d10fa9807bb2f
TheJavier1745 <javierdiazyevenes2002@gmail.com>
2025-02-17 10:26:24 -0300

    tabla usuarios ajustada pc

--- PracticaAstro/Astro/src/components/TablaAdministracionUsuarios.jsx
+++ PracticaAstro/Astro/src/components/TablaAdministracionUsuarios.jsx
@@ -4,7 +4,6 @@ import { Button, IconButton, Dialog, DialogActions, DialogContent, DialogTitle, 
 import DeleteIcon from '@mui/icons-material/Delete';
 import Box from '@mui/material/Box';
 import CircularProgress from '@mui/material/CircularProgress';
-import Swal from 'sweetalert2';
 import ArrowBackIcon from '@mui/icons-material/ArrowBack';
 
 const TablaAdministracionUsuarios = () => {
@@ -124,18 +123,20 @@ const TablaAdministracionUsuarios = () => {
 
   return (
     <Box sx={{ width: '90%', maxWidth: '1200px', margin: '0 auto', padding: 2 }}>
-      <p>
-        <h1>Administración de usuarios</h1>
-        Bienvenido al panel de administración de usuarios. Aquí podrás administrar
-        los roles que tenga cada usuario registrado.
-        <br />
-        <b>Cualquier cambio realizado no será notificado al usuario en cuestión.</b>
-      </p>
-      <div style={{ display: "flex", justifyContent: "space-between", marginTop: "20px", gap: "10px" }}>
+      <Box sx={{ marginBottom: 2 }}>
+        <h1 style={{ fontSize: '2rem', margin: '0.5em 0' }}>Administración de usuarios</h1>
+        <p style={{ fontSize: '1rem', margin: '0.5em 0' }}>
+          Bienvenido al panel de administración de usuarios. Aquí podrás administrar
+          los roles que tenga cada usuario registrado.
+          <br />
+          <b>Cualquier cambio realizado no será notificado al usuario en cuestión.</b>
+        </p>
+      </Box>
+      <Box sx={{ display: 'flex', justifyContent: 'space-between', marginTop: '20px', gap: '10px' }}>
         <Button
           variant="contained"
           color="error"
-          style={{ padding: "10px 20px", cursor: "pointer" }}
+          sx={{ padding: '10px 20px', cursor: 'pointer' }}
           onClick={() => {
             window.location.href = "/admin";
           }}
@@ -143,7 +144,7 @@ const TablaAdministracionUsuarios = () => {
         >
           Regresar sin hacer cambios
         </Button>
-      </div>
+      </Box>
       {loading ? (
         <Box sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100%' }}>
           <CircularProgress />
@@ -164,6 +165,14 @@ const TablaAdministracionUsuarios = () => {
               },
               '& .MuiDataGrid-columnHeaders': {
                 fontSize: '1.4rem',
+              },
+              '@media (max-width: 600px)': {
+                '& .MuiDataGrid-cell': {
+                  fontSize: '1rem',
+                },
+                '& .MuiDataGrid-columnHeaders': {
+                  fontSize: '1rem',
+                },
               },
             }}
           />
